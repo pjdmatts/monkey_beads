1e545bc751b69ddd6650c3570b59b2c1011a4749
Peter Matthews <pjdmatthews7@gmail.com>
2017-03-12 19:09:42 -0400

    first commit

---
+++ entertainment_center.py
@@ -0,0 +1,36 @@
+import media
+import fresh_tomatoes
+
+toy_story = media.Movie("Toy Story", "The Toys Are Alive?!",
+                        "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+                        "https://www.youtube.com/watch?v=vwyZH85NQC4")
+
+avatar = media.Movie("Avatar",
+                     "A Marine Goes Camping With Some Tall Blue People",
+                     "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+                     "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+
+north_by_northwest = media.Movie("North By Northwest",
+                                 "Running Away From Planes While Keeping Hair Nice",
+                                 "https://upload.wikimedia.org/wikipedia/commons/8/83/Northbynorthwest1.jpg",
+                                 "https://www.youtube.com/watch?v=VZmbbx2p4yI")
+
+big_lebowski = media.Movie("The Big Lebowski",
+                           "Bowling, Cardigans and Beards",
+                           "https://upload.wikimedia.org/wikipedia/en/3/35/Biglebowskiposter.jpg",
+                           "https://www.youtube.com/watch?v=VgSqm8-wXWA")
+
+the_martian = media.Movie("The Martian",
+                          "Potatoes, Anyone?",
+                          "https://upload.wikimedia.org/wikipedia/en/c/cd/The_Martian_film_poster.jpg",
+                          "https://www.youtube.com/watch?v=XQnYm5MG1x0")
+
+harry = media.Movie("The Trouble With Harry",
+                    "How To Dispose Of A Corpse In Beautiful Vermont",
+                    "https://upload.wikimedia.org/wikipedia/commons/c/c2/The_Trouble_with_Harry.jpg",
+                    "https://www.youtube.com/watch?v=U0w_1cHIc-0")
+
+movies = [toy_story, avatar, north_by_northwest,
+          big_lebowski, the_martian, harry]
+
+fresh_tomatoes.open_movies_page(movies)
---
+++ readme.md
@@ -0,0 +1,40 @@
+**Entertainment Center**
+
+***What is this code for?***
+
+`entertainment_center.py` is a script that allows you to store information
+about your favorite movies (using a class set up in `media.py`) and have the
+`fresh_tomatoes.py` module build a web page that showcases the movies
+and allows the use to view the movies trailers.
+
+***Files Included***
+
+- `fresh_tomatoes.py` a script to generate a web page that shows the movies
+and their trailers.
+
+- `main.css` custom css that overrides some of the Bootstrap defaults
+
+- `entertainment_center.py` a script to store information about the movies
+
+- `media.py` a class that provides a way to store movie related information
+
+***Requirements***
+
+This code was written with Python 2.7.13.
+
+***How to use this application***
+
+Navigate to the root of the application and in a terminal type:
+```
+python entertainment_center.py
+
+```
+
+***ToDo***
+
+- Modify the Bootstrap Navbar. Hoping that the next step of the course will
+cover this.
+
+- Add an Admin Interface to allow updates to the movies from the browser.
+
+- Add access to [IMBD database](http://www.imdb.com/)
